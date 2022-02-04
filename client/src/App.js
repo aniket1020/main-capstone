20b1a1ea85f83d14e46e927c8ff7d6ab92eea142
Aniket Inamdar <aniket.1020@gmail.com>
2022-02-04 18:32:13 +0530

    Added dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
+      {/* Note : this is just a setup, please reconfigure */}
       Hello
+      <Routes>
+        <Route path='/home' element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ client/src/components/Home.js
@@ -0,0 +1,6 @@
+function Home()
+{
+    return (<div>Hello Home</div>)
+}
+
+export default Home;
