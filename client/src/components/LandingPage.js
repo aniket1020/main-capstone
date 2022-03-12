c758e57a7b0543d154f7ebed04619adc257da818
Dhwanit Kapur <dhwanitkapur.dk@gmail.com>
2022-03-13 04:21:41 +0530

    Completed a couple of sections

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,9 +1,69 @@
 import React from "react";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import Card from "./Card";
+import "./css/LandingPage.css";
+import LandingImg from "./images/Landing-cover.png";
+import MetamaskImg from "./images/metamask.jpeg";
+import CoinbaseImg from "./images/coinbase.jpeg";
+import BinanceImg from "./images/binance.png";
 
 const LandingPage = () => {
   return (
-    <div>
-      <h1>Landing Page...</h1>
+    <div className="landing">
+      <NavBar />
+      <div className="landing-cover">
+        <div className="landing-cover-left">
+          <h1>Explore the Best</h1>
+          <h1 id="alt-text">NFT World</h1>
+          <p>Collect and Sell your Extraordinary Artwork.</p>
+          <div className="landing-cover__buttons">
+            <a id="alt-button" href="/">
+              Explore
+            </a>
+            <a href="/">Upload</a>
+          </div>
+        </div>
+        <div className="landing-cover-right">
+          <img src={LandingImg} alt="Landing cover img not showing" />
+        </div>
+      </div>
+      <div className="landing-wallets">
+        <p>--Wallet</p>
+        <h3>Wallets We Support</h3>
+        <div className="landing-wallets__cards">
+          <div className="wallets-indi">
+            <img
+              className="wallet-img"
+              src={MetamaskImg}
+              alt="Metamask img not available"
+            />
+            <p className="wallet-card__text">Metamask</p>
+          </div>
+          <div className="wallets-indi">
+            <img
+              className="wallet-img"
+              src={CoinbaseImg}
+              alt="Coinbase img not available"
+            />
+            <p className="wallet-card__text">Coinbase</p>
+          </div>
+          <div className="wallets-indi">
+            <img
+              className="wallet-img"
+              src={BinanceImg}
+              alt="Binance img not available"
+            />
+            <p className="wallet-card__text">Binance Wallet</p>
+          </div>
+        </div>
+      </div>
+      <div className="landing-auctions">
+        <p>--Live Auction</p>
+        <h3>Trending Auctions</h3>
+      </div>
+
+      <Footer />
     </div>
   );
 };
