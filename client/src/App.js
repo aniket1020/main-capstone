ced9337df8dbe8d027cd5236a61e928ca959340c
Dhwanit Kapur <dhwanitkapur.dk@gmail.com>
2022-03-13 01:08:57 +0530

    Setting up remote branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
-import UserProfile from './components/UserProfile';
+import UserProfile from "./components/UserProfile";
+import LandingPage from "./components/LandingPage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path='/userProfile' element={<UserProfile />} />
+        <Route exact path="/userProfile" element={<UserProfile />} />
+        <Route exact path="/" element={<LandingPage />} />
       </Routes>
     </div>
   );
