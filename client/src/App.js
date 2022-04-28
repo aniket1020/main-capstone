b8b05fd7c9e059d4de38ffba8394c31646d292f1
ojas.madan <ojas.madan06@gmail.com>
2022-04-28 16:05:31 +0530

    wallet page

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { Routes, Route } from "react-router-dom";
 
 import UserProfile from './components/UserProfile';
+import Wallet from './components/Wallet';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route exact path='/userProfile' element={<UserProfile />} />
+        <Route exact path='/wallet' element={<Wallet />} />
       </Routes>
     </div>
   );
---
+++ client/src/components/Wallet.js
@@ -0,0 +1,41 @@
+import './css/Wallet.css'
+import NavBar from './NavBar'
+import Footer from './Footer'
+
+function Wallet()
+{
+
+return (    
+    <>
+    <NavBar />
+    <div className='Header'>
+        <div className='Header__row1'>
+        | <br />
+        Explore{/* #F18A24 */}
+    </div>
+    <div className='Header__row2'>Connect Your Wallet</div>
+    <div className='Header__row3'> Home {'>'} <span>Wallet</span> </div>
+    <div className='walletSection'>
+        <div className='walletRow'>
+            <div className='wallet'>
+                <img className='walletImage' src={require('./images/metamask.png') } alt=""></img>
+                <div className='walletName'>Metamask</div>
+            </div>
+            <div className='wallet'>
+                <img className="walletImage" src={require('./images/coinbase.png') } alt=""></img>
+                <div className='walletName'>Coinbase</div>
+            </div>
+            <div className='wallet'>
+                <img className="walletImage" src={require('./images/binance.png') } alt=""></img>
+                <div className='walletName'>Binance</div>
+            </div>
+        </div>
+    </div>
+
+    </div>
+    <Footer/>
+    </>
+    )
+}
+
+export default Wallet
