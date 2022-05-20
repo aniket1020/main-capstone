08d290e1fe5b6fc1e37ec3099edfe947b6563316
Aniket Inamdar <aniket.1020@gmail.com>
2022-05-20 09:11:53 +0530

    Bad merge, resolve conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -8,8 +8,6 @@ import Explore from "./components/Explore";
 import LandingPage from "./components/LandingPage";
 import UploadNft from "./components/UploadNft";
 
-import { useState } from "react";
-
 import store from './store';
 import { Provider } from 'react-redux';
 
@@ -22,51 +20,7 @@ import NFTAbi from "./contractsData/NFT.json";
 import MarketplaceAddress from "./contractsData/NFT-address.json";
 import MarketplaceAbi from "./contractsData/NFT.json";
 
-import { ethers } from 'ethers';
-import NFTAddress from './contractsData/NFT-address.json';
-import NFTAbi from './contractsData/NFT.json';
-import MarketplaceAddress from './contractsData/NFT-address.json';
-import MarketplaceAbi from './contractsData/NFT.json';
-
 function App() {
-  const [account, setAccount] = useState(null);
-  const [nft, setNFT] = useState({});
-  const [marketplace, setMarketplace] = useState({});
-  // MetaMask Login/Connect
-  const web3Handler = async () => {
-    console.log("Inside web3handler function!!!");
-    const accounts = await window.ethereum.request({
-      method: "eth_requestAccounts",
-    });
-    setAccount(accounts[0]);
-    // Get provider from Metamask
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    // Set signer
-    const signer = provider.getSigner();
-
-    window.ethereum.on("chainChanged", (chainId) => {
-      window.location.reload();
-    });
-
-    window.ethereum.on("accountsChanged", async function (accounts) {
-      setAccount(accounts[0]);
-      await web3Handler();
-    });
-    loadContracts(signer);
-  };
-  const loadContracts = async (signer) => {
-    // Get deployed copies of contracts
-    const marketplace = new ethers.Contract(
-      MarketplaceAddress.address,
-      MarketplaceAbi.abi,
-      signer
-    );
-    setMarketplace(marketplace);
-    const nft = new ethers.Contract(NFTAddress.address, NFTAbi.abi, signer);
-    console.log("nfttyuiop: ", nft);
-    setNFT(nft);
-  };
-
   let persistor = persistStore(store);
 
   const [nftInstance, setNftInstance] = useState(null);
